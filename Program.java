0d2a61d82970f57525fb07104760811e1b2d0533
phansonloc1999 <phansonloc1999@gmail.com>
2022-03-16 10:38:13 +0700

    Add search slang

--- Program.java
+++ Program.java
@@ -7,10 +7,24 @@ import java.util.Scanner;
  * Program
  */
 public class Program {
+    private static Hashtable<String, String[]> my_dict;
 
-    public static void main(String[] args) {
-        Hashtable<String, String[]> my_dict = new Hashtable<String, String[]>();
-        Scanner scanner;
+    private static void searchSlang() {
+        System.out.println("=======================Tim kiem slang=======================");
+        System.out.print("Nhap tu khoa slang can tim: ");
+        Scanner scanner = new Scanner(System.in);
+        String slang = scanner.nextLine();
+        String[] meanings = my_dict.get(slang);
+        if (meanings.length > 0) {
+            System.out.println("Y nghia cua tu " + slang + " la :");
+            for (String meaning : meanings) {
+                System.out.println(meaning);
+            }
+        }
+    }
+
+    private static void readSlangsFromFile() {
+        Scanner scanner = null;
         try {
             scanner = new Scanner(new File("Data/slang,txt"), "UTF-8");
             scanner.nextLine(); // Skip first line
@@ -20,16 +34,21 @@ public class Program {
                 String[] oldTokens = tokens;
                 tokens = line.split("`");
                 if (tokens.length == 2) {
-                    System.out.println(tokens[0] + ": " + tokens[1]);
                     my_dict.put(tokens[0], new String[] { tokens[1] });
                 } else {
                     my_dict.put(oldTokens[0], new String[] { oldTokens[1], tokens[0] });
-                    System.out.println(tokens[0]);
                 }
             }
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        my_dict = new Hashtable<String, String[]>();
+        readSlangsFromFile();
+        searchSlang();
     }
 }
