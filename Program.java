fdf1ff2e08f6c9e49bb99e4e4f4d511ae268fac5
phansonloc1999 <phansonloc1999@gmail.com>
2022-03-16 10:29:17 +0700

    Read and print slang & meaning from file

--- Program.java
+++ Program.java
@@ -1,9 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Hashtable;
+import java.util.Scanner;
+
 /**
  * Program
  */
 public class Program {
 
     public static void main(String[] args) {
-
+        Hashtable<String, String[]> my_dict = new Hashtable<String, String[]>();
+        Scanner scanner;
+        try {
+            scanner = new Scanner(new File("Data/slang,txt"), "UTF-8");
+            scanner.nextLine(); // Skip first line
+            String[] tokens = null;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] oldTokens = tokens;
+                tokens = line.split("`");
+                if (tokens.length == 2) {
+                    System.out.println(tokens[0] + ": " + tokens[1]);
+                    my_dict.put(tokens[0], new String[] { tokens[1] });
+                } else {
+                    my_dict.put(oldTokens[0], new String[] { oldTokens[1], tokens[0] });
+                    System.out.println(tokens[0]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
